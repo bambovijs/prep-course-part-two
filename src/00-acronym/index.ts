8a18f6894be7cccc90b63e7cd11fd6d3cb5093df
Raivo Bambis <raivo@bambis.lv>
2024-01-21 18:47:37 +0200

    01

--- src/00-acronym/index.ts
+++ src/00-acronym/index.ts
@@ -6,6 +6,21 @@
  * Help generate some jargon by writing a program that converts a long name like Portable Network Graphics to its acronym (PNG).
  */
 
-function parse(input: string) {}
+function parse(input: string) {
+  let result = "";
+  const words = input.split(/[ -]/g);
+  //console.log(words);
+
+  words.forEach((word) => {
+    const cleanWord = word.replace(/[^a-z]/gi, "");
+    //console.log(cleanWord);
+
+    if (cleanWord) {
+      result += cleanWord[0].toUpperCase();
+    }
+  });
+
+  return result;
+}
 
 export { parse };
