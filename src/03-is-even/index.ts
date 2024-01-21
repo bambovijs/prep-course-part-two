79e1fc20009c6a53f8475f3c4dcc9ade4809df04
Raivo Bambis <raivo@bambis.lv>
2024-01-21 19:36:06 +0200

    03

--- src/03-is-even/index.ts
+++ src/03-is-even/index.ts
@@ -9,6 +9,18 @@
  * isEven(3) === false
  */
 
-function isEven(n: number) {}
+const EVEN_NUMS = [0, 2, 4, 6, 8];
+
+function isEven(n: number) {
+  if (n !== 0) {
+    const nAsString = n.toString(); // parvesh par string
+    const wholeNumAsString = nAsString.split(".")[0]; // splito pie decimaldaļām
+    const lastChar = wholeNumAsString[wholeNumAsString.length - 1];
+
+    return EVEN_NUMS.includes(Number(lastChar));
+  } else {
+    return false;
+  }
+}
 
 export { isEven };
