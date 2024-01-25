e9a85d069b7e55c451374de443ec6c6de24ee429
Raivo Bambis <raivo@bambis.lv>
2024-01-25 14:51:39 +0200

    13 - 1 test not passed

--- src/13-high-scores/index.ts
+++ src/13-high-scores/index.ts
@@ -1,7 +1,11 @@
 /**
  * Manage a game player's High Score list.
  *
- * Your task is to build a high-score component of the classic Frogger game, one of the highest selling and addictive games of all time, and a classic of the arcade era. Your task is to write methods that return the highest score from the list, the last added score and the three highest scores.
+ * Your task is to build a high-score component of the classic Frogger game,
+ *  one of the highest selling and addictive games of all time,
+ *  and a classic of the arcade era.
+ *  Your task is to write methods that return the highest score from the list,
+ *  the last added score and the three highest scores.
  */
 
 class HighScores {
@@ -11,15 +15,35 @@ class HighScores {
   }
 
   get latest() {
-    return 0;
+    if (this.scores.length === 0) {
+      console.log("Empty array");
+    }
+
+    return this.scores.pop();
   }
 
   get personalBest() {
-    return 0;
+    let temp = this.scores[0];
+
+    if (this.scores.length !== 0) {
+      for (let i = 0; i < this.scores.length; i++) {
+        if (temp < this.scores[i]) {
+          temp = this.scores[i];
+        }
+      }
+    } else {
+      throw new Error("Scores array is empty");
+    }
+
+    return temp;
   }
 
   get personalTopThree() {
-    return 0;
+    let sortedArray = this.scores.slice().sort((a, b) => b - a);
+    let resultArray: number[] = sortedArray.slice(0, 3);
+    console.log(sortedArray);
+
+    return resultArray;
   }
 }
 
