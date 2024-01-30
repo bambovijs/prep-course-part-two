028a5787381620c30efd45bf4cba719bd2060555
Raivo Bambis <raivo@bambis.lv>
2024-01-30 11:14:17 +0200

    16

--- src/16-second-largest/index.ts
+++ src/16-second-largest/index.ts
@@ -4,6 +4,11 @@
  * Array of numbers are passed in the function, your task is to find the second largest number.
  */
 
-function secondLargest(array: number[]) {}
+function secondLargest(array: number[]): number {
+  const sortedArray = array.slice().sort((a, b) => b - a);
+  const secondNumber = sortedArray[1];
+
+  return secondNumber;
+}
 
 export { secondLargest };
