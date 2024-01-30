02ad57a92af283e9d3bda64b24b026d53b32ccc6
Raivo Bambis <raivo@bambis.lv>
2024-01-30 12:57:21 +0200

    17

--- src/17-max-char/index.ts
+++ src/17-max-char/index.ts
@@ -9,6 +9,24 @@
  * maxChar("apple 1231111") === "1"
  */
 
-function maxChar(str: string) {}
+function maxChar(str: string): string {
+  const charMap: { [key: string]: number } = {};
+
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  let maxChar = "";
+  let maxCount = 0;
+
+  for (let char in charMap) {
+    if (charMap[char] > maxCount) {
+      maxChar = char;
+      maxCount = charMap[char];
+    }
+  }
+
+  return maxChar;
+}
 
 export { maxChar };
